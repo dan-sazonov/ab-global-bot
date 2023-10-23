a61d577e8b533218c976a65b2ba9fd242bb25734
Dan Sazonov <p.294803@gmail.com>
2023-10-23 14:31:23 +0300

    feat: check the existence of data file

--- bot/services.py
+++ bot/services.py
@@ -1,11 +1,22 @@
-def _file_to_list(path: str) -> list[str]:
+import os.path
+
+
+def _check_data_file(path: str):
+    if not os.path.isfile(path):
+        print("Can't read data file with words")
+        exit(1)
+
+
+def get_words() -> list[str]:
     """
     Converts a txt file with words on separate lines to a list of strings.
     Empty strings would be deleted
 
-    :param path: absolute or relative path to the txt file
     :return: list of str
     """
+    path = '../words.txt'
+    _check_data_file(path)
+
     with open(path, 'r', encoding='utf-8') as f:
         file_content = f.read().split('\n')
         words = list(filter(None, file_content))  # remove empty strings
