6ca318efe79ef8c10ed2f3ab889d16796d589bca
Dan Sazonov <p.294803@gmail.com>
2023-10-23 18:21:28 +0300

    feat: sample of functions

--- bot/db.py
+++ bot/db.py
@@ -1,6 +1,19 @@
 from models import models_list, db
 
 
+def _has_db() -> bool:
+    pass
+
+
+def _add_words() -> None:
+    pass
+
+
 def create_tables():
+    db_prev_state = _has_db()
+
     with db:
         db.create_tables(models_list)
+
+    if not db_prev_state:
+        _add_words()
