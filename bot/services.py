8bd10eeec14aab60f2384310cd2674aefaf7ea7b
Dan Sazonov <p.294803@gmail.com>
2023-10-23 19:12:46 +0300

    feat: add creator of words objects

--- bot/services.py
+++ bot/services.py
@@ -7,7 +7,7 @@ def _check_data_file(path: str):
         exit(1)
 
 
-def get_words() -> list[str]:
+def _get_words() -> list[str]:
     """
     Converts a txt file with words on separate lines to a list of strings.
     Empty strings would be deleted
@@ -20,5 +20,20 @@ def get_words() -> list[str]:
     with open(path, 'r', encoding='utf-8') as f:
         file_content = f.read().split('\n')
         words = list(filter(None, file_content))  # remove empty strings
+
+    return words
+
+
+def get_words_objects() -> list[dict]:
+    word_list = _get_words()
+    words = []
+
+    for index, _word in enumerate(word_list, start=1):
+        words.append({
+            'id': index,
+            'word': _word,
+            'show_num': 0,
+            'vote_num': 0
+        })
 
     return words
