74483b868b61adde6218eb52ca4b21f81b9f323f
Dan Sazonov <p.294803@gmail.com>
2023-10-23 13:53:59 +0300

    refactor: models in another module

--- bot/db.py
+++ bot/db.py
@@ -1,39 +1,10 @@
-import peewee as pw
-
-db = pw.SqliteDatabase('data.db')
-
-
-class BaseModel(pw.Model):
-    class Meta:
-        database = db
-
-
-class User(BaseModel):
-    id = pw.PrimaryKeyField(unique=True)
-    tg_id = pw.BigIntegerField(unique=True)
-    date_reg = pw.DateTimeField()
-    date_act = pw.DateTimeField()
-    resp_num = pw.IntegerField()
-
-    class Meta:
-        db_table = 'users'
-        order_by = 'resp_num'
-
-
-class Word(BaseModel):
-    id = pw.PrimaryKeyField(unique=True)
-    word = pw.CharField()
-    show_num = pw.IntegerField()
-    vote_num = pw.IntegerField()
-
-    class Meta:
-        db_table = 'words'
+from models import models_list, db
 
 
 def _create_db():
     # only for tests
     with db:
-        db.create_tables([User, Word])
+        db.create_tables(models_list)
 
 
 _create_db()
---
+++ bot/models.py
@@ -0,0 +1,33 @@
+from peewee import Model, PrimaryKeyField, BigIntegerField, DateTimeField, IntegerField, CharField, SqliteDatabase
+
+db = SqliteDatabase('data.db')
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class User(BaseModel):
+    id = PrimaryKeyField(unique=True)
+    tg_id = BigIntegerField(unique=True)
+    date_reg = DateTimeField()
+    date_act = DateTimeField()
+    resp_num = IntegerField()
+
+    class Meta:
+        db_table = 'users'
+        order_by = 'resp_num'
+
+
+class Word(BaseModel):
+    id = PrimaryKeyField(unique=True)
+    word = CharField()
+    show_num = IntegerField()
+    vote_num = IntegerField()
+
+    class Meta:
+        db_table = 'words'
+
+
+models_list = [User, Word]
